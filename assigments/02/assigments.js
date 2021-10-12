1780393e2ea63c9ae3c83a7e3c23710c25ab0cfe
gkiss <gergely@20shots.co.uk>
2021-10-12 22:05:27 +0100

    assigments

--- assigments/02/assigments.js
+++ assigments/02/assigments.js
@@ -172,18 +172,96 @@ method will set a new property on the object, called 'isIsland'.
 'isIsland' will be true if there are no neighbouring countries, and false if
 there are. Use the ternary operator to set the property.*/
 
-console.log(myCountry.describe() + `${myCountry.checkIsland()? ` and is an island`: `and is not an island`}`);
+console.log(myCountry.describe() + `${myCountry.checkIsland() ? ` and is an island` : `and is not an island`}`);
 
+/*
+Iteration: The for Loop
+1. There are elections in your country! In a small town, there are only 50 voters.
+Use a for loop to simulate the 50 people voting, by logging a string like this to
+the console (for numbers 1 to 50): 'Voter number 1 is currently voting'
+*/
 
+//for (let i = 1; i <= 50; i++) {
+//    console.log(`Voter number ${i} is currently voting`);
+//}
 
+/*
+Looping Arrays, Breaking and Continuing
+1. Let's bring back the 'populations' array from a previous assignment
+2. Use a for loop to compute an array called 'percentages2' containing the
+percentages of the world population for the 4 population values. Use the
+function 'percentageOfWorld1' that you created earlier
+3. Confirm that 'percentages2' contains exactly the same values as the
+'percentages' array that we created manually in the previous assignment,
+and reflect on how much better this solution is
+*/
 
+const percentages2 = []
+for (let i = 0; i < populations.length; i++) {
+    percentages2.push(percentageOfWorld1(populations[i]))
+}
+let isValid = true;
+for (let i = 0; i < percentages2.length; i++) {
+    if (percentages[i] !== percentages2[i]) {
+        console.log(`Error at ${percentages[i]} index percentages: ${i} percentages2: ${percentages2[i]}`);
+        isValid = false;
+        break;
+    }
+}
+if (isValid) {
+    console.log(`Valid processing!`);
+}
 
+/*
+Looping Backwards and Loops in Loops
+1. Store this array of arrays into a variable called 'listOfNeighbours'
+[['Canada', 'Mexico'], ['Spain'], ['Norway', 'Sweden',
+'Russia']];
+2. Log only the neighbouring countries to the console, one by one, not the entire
+arrays. Log a string like 'Neighbour: Canada' for each country
+3. You will need a loop inside a loop for this. This is actually a bit tricky, so don't
+worry if it's too difficult for you! But you can still try to figure this out anyway �
+*/
+const listOfNeighbours = [['Canada', 'Mexico'], ['Spain'], ['Norway', 'Sweden', 'Russia']];
 
+for (let i = 0; i < listOfNeighbours.length; i++) {
+    if (listOfNeighbours[i].length > 1) {
+        for (let j = 0; j < listOfNeighbours[i].length; j++) {
+            for (let k = 0; k < listOfNeighbours[i].length; k++) {
+                if (j !== k)
+                    console.log(`Neighbour country for ${listOfNeighbours[i][j]}: ${listOfNeighbours[i][k]}`);
+            }
+        }
+    } else {
+        console.log(`Country ${listOfNeighbours[i]} have no neighbour`);
+    }
+}
 
+/*
+The while Loop
+1. Recreate the challenge from the lecture 'Looping Arrays, Breaking and Continuing',
+but this time using a while loop (call the array 'percentages3')
+2. Reflect on what solution you like better for this task: the for loop or the while
+loop
+*/
 
-
-
-
+const percentages3 = []
+let i = 0;
+while ( i < populations.length) {
+    percentages3.push(percentageOfWorld1(populations[i]))
+    i++
+}
+isValid = true;
+for (let i = 0; i < percentages3.length; i++) {
+    if (percentages[i] !== percentages3[i]) {
+        console.log(`Error at ${percentages[i]} index percentages: ${i} percentages2: ${percentages3[i]}`);
+        isValid = false;
+        break;
+    }
+}
+if (isValid) {
+    console.log(`Valid processing!`);
+}
 
 
 
