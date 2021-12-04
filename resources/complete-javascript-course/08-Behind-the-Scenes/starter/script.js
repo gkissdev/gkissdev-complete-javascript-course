8dbb4ec7869c46bbce5dd3c0e3717a26ec57fc19
gkiss.dev <66020992+gkissdev@users.noreply.github.com>
2021-12-04 20:11:09 +0000

    messing about with scopes

--- resources/complete-javascript-course/08-Behind-the-Scenes/starter/script.js
+++ resources/complete-javascript-course/08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+function calcAge(birthYear){
+    const demoYear = 2037
+    const age = demoYear - birthYear
+    function printAge(){
+        const firstName = 'GG'
+        let  output  = `${firstName} you are ${age} born in ${birthYear} in the year ${demoYear}`
+        console.log(output)
+        if(birthYear >= 1981 && birthYear<= 1996){
+            const str = `Oh you are a millennial, ${firstName}`
+            console.log(str);
+            function add(a, b){
+                return a+ b
+            }
+            output = 'NEW'
+            console.log(output);
+        }
+        console.log(output);
+    }
+    printAge()
+    return age
+}
+const firstName = 'Gergely'
+calcAge(1983)
